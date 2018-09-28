dab6f462de9221f78bb192047b5954aaf41660b3
Gregory <huyghegregory@yahoo.fr>
2018-09-28 12:28:56 +0200

    Set the blog part, author, category and date archives, custom quesries

--- wp-content/themes/fictional-university-theme/archive.php
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -6,10 +6,12 @@ get_header(); ?>
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title">
-                Welcome to our blog
+               
+               <?php the_archive_title(); ?>
+              
             </h1>
             <div class="page-banner__intro">
-                <p>Keep up with our latest news</p>
+                <p><?php the_archive_description(); ?></p>
             </div>
         </div>
     </div>
