6a3638147a0cb14916ef0685407c9b8ccf57e16f
Gregory <huyghegregory@yahoo.fr>
2018-10-03 14:50:22 +0200

    create a function to reduce code, and use get_template-part()

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,37 @@
 <?php
 
+function pageBanner ($args = NULL) {
+    
+    if (!$args['title']) {
+        $args['title'] = get_the_title();
+    }
+    
+    if (!$args['subtitle']) {
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+    
+    if (!$args['photo']) {
+        if (get_field('page_banner_background_image')) {
+            $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+    
+    ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>)"></div>
+            <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle'] ?></p>
+            </div>
+        </div>
+    </div>
+<?php }
+
+
+
 function university_files() {
     
     wp_enqueue_script('javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -5,19 +5,9 @@ get_header(); ?>
 <?php
     
     while(have_posts()) {
-        the_post(); ?>
-
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">
-                <?php the_title();?>
-            </h1>
-            <div class="page-banner__intro">
-                <p>REPLACE LATER</p>
-            </div>
-        </div>
-    </div>
+        the_post(); 
+        pageBanner();
+        ?>
 
     <div class="container container--narrow page-section">
 
@@ -93,37 +83,11 @@ get_header(); ?>
         echo '<h2 class="headline headline--medium"> Upcoming ' . get_the_title() . ' Events </h2>';
 
         while($homepageEvents->have_posts()) {
-            $homepageEvents -> the_post(); ?>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month">
-                
-                <?php 
-                $the_event_date = get_field( 'event_date', false, false );
+            $homepageEvents -> the_post();
             
-                // THEN create object
-                $the_event_date = new DateTime( $the_event_date );
-                echo $the_event_date->format( 'M' );
-                ?>
-                </span>
-                
-                <span class="event-summary__day"><?php echo $the_event_date->format( 'd' ); ?></span>  
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
-                    <?php the_title(); ?></a></h5>
-                    <p>
-                    <?php if (has_excerpt()) {
-                    echo get_the_excerpt();
-                    } else {
-                    echo wp_trim_words(get_the_content(), 18);
-                    }
-                    ?>
-                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                    </p>
-                </div>
-            </div>
-        <?php }
+            get_template_part('template-parts/content', get_post_type());
+            
+        }
     }
 
     ?>
